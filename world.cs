5caaf6ba3f2e27404336566ea3ff2850bdf54420
Archer Wayne <archerwayne1216@gmail.com>
2023-06-10 10:58:37 +0800

    Update world.cs

--- world.cs
+++ world.cs
@@ -6,7 +6,7 @@ public partial class world : Node2D
 	PackedScene packedscene;
 	public override void _Ready()
 	{
-		
+		packedscene = (PackedScene)GD.Load("res://node_2d.tscn");
 	}
 
 	public override void _Process(double delta)
@@ -17,6 +17,10 @@ public partial class world : Node2D
 	{
 		if (@event is InputEventMouseButton mouseEvent){
 			// create instance of sprite scene
+			GD.Print("reached mouse event callback");
+			Sprite2D sprite = (Sprite2D)packedscene.Instantiate();
+			sprite.Position = mouseEvent.Position;
+			this.AddChild(sprite);
 
 		}
 	}
